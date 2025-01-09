951f345c1da715020b183b91734878d89c98a384
Ritam <ritamdevs2004@gmail.com>
2025-01-09 23:23:36 +0530

    last/commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   typescript: {
     ignoreBuildErrors: true, // Consider fixing TypeScript errors for better stability
   },
